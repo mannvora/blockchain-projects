4c2ddc65bdca66599042df2f992bdeed5c84ab41
Mann_Vora1729 <55322576+mannvora@users.noreply.github.com>
2024-04-10 21:15:45 -0700

    Add files via upload

---
+++ erc-721.sol
@@ -0,0 +1,39 @@
+// contracts/GameItem.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract GameItem is ERC721 {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    string private constant _name = "stale-paste";
+    string private constant _symbol = "-A-ASAPEAS";
+    string private constant _message =
+        "Sustainable blockchain practices - mvora14";
+
+    constructor() ERC721(_name, _symbol) {}
+
+    function name() public view virtual override returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view virtual override returns (string memory) {
+        return _symbol;
+    }
+
+    function message() public view returns (string memory) {
+        return _message;
+    }
+
+    function awardItem(address player) public returns (uint256) {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(player, newItemId);
+
+        return newItemId;
+    }
+}
